7713201f8bf715a4d4d86a29153a0e1fdf351b49
RealZN <71386797+TbhX@users.noreply.github.com>
2026-05-02 15:54:29 +0200

    Update page.tsx

--- src/app/(main)/playlist/[id]/page.tsx
+++ src/app/(main)/playlist/[id]/page.tsx
@@ -1,1 +1,4 @@
-export default function PlaylistPage({ params }: { params: { id: string } }) { return <div className='p-8'>Playlist {params.id}</div> }
+export default async function PlaylistPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  return <div className='p-8'>Playlist {id}</div>
+}
